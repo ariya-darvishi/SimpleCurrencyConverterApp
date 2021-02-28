96c809078c132ecb1308e005b9fbf3ac4c6c8305
Amirreza.dq <amirreza.dq@gmail.com>
2021-02-28 19:28:17 +0330

    Setup view binding in MainActivity :)

--- app/src/main/java/com/example/simplecurrencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/simplecurrencyconverterapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.simplecurrencyconverterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simplecurrencyconverterapp.databinding.ActivityMainBinding
+import javax.net.ssl.SSLSessionBindingEvent
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
